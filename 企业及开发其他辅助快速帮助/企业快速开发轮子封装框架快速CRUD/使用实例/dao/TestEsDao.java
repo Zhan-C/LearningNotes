fbc3318491a129bc233f0ef91d8129ac865fcf5b
AChuang <zhanc996@foxmail.com>
2021-01-19 19:29:09 +0800

    删除快速开发中的文件

--- 企业及开发其他辅助快速帮助/企业快速开发轮子封装框架快速CRUD/使用实例/dao/TestEsDao.java
+++
@@ -1,7 +0,0 @@
-package com.kang.shop.usertestserver.dao;
-
-import com.kang.shop.es.base.BaseEsDao;
-import com.kang.shop.usertestserver.entity.TestEs;
-
-public interface TestEsDao extends BaseEsDao<TestEs,String> {
-}
--- 企业及开发其他辅助快速帮助/企业快速开发轮子封装框架快速CRUD/使用实例/service/TestJpaService.java
+++
@@ -1,7 +0,0 @@
-package com.kang.shop.usertestserver.service;
-
-import com.kang.shop.jpa.base.BaseJpaService;
-import com.kang.shop.usertestserver.entity.TestJpa;
-
-public interface TestJpaService extends BaseJpaService<TestJpa,Long> {
-}
